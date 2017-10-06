cc0abb97a02c102fd9fa148619b168beebee8973
Juho Friman <juho.friman@solita.fi>
2017-10-06 16:16:07 +0300

    Disable signing for now

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://github.com/juhofriman/leetconfig"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :deploy-repositories [["clojars"
+                         {:url "https://clojars.org/repo/"
+                          :sign-releases false}]])
