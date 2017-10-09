f4000c96427449fd9cfcae9b3e215d509c0d3f89
Juho Friman <juho.friman@solita.fi>
2017-10-09 16:28:16 +0300

    Slight cleanup

--- src/leet/config.clj
+++ src/leet/config.clj
@@ -8,7 +8,7 @@
     (val key)
     val))
 
-(defn c [env key default & overrides]
+(defn select-value [env key default & overrides]
   (let [o (into {} (map vec (partition 2 overrides)))]
     (if-let [e (get o env)]
       (if (associative? default)
@@ -16,6 +16,14 @@
         (call-if-fn key e))
       (call-if-fn key default))))
 
-(defmacro leetconfig [env-key & defs]
+(defmacro leetconfig
+  "Evaluates body to form a map. First argument is the used environment key
+   such as :test or :production. Rest arguments are key-value constraints such as
+   (:key \"default value\" :env-1 \"Override value\") If value is a fn, it is
+   called with key as an argument"
+  [env-key & defs]
   (let [ek (eval env-key)]
-    (into {} (map (fn [[key default & overrides]] (vector key `(c ~ek ~key ~default ~@overrides))) defs))))
+    (into {} (map
+               (fn [[key default & overrides]]
+                 (vector key `(select-value ~ek ~key ~default ~@overrides)))
+               defs))))
