0fa6d3c4aa05e581032f788e1db92481d1fc5eed
Juho Friman <juho.t.friman@gmail.com>
2017-12-06 18:41:13 +0200

    Preparing 0.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leetconfig "0.0.3"
+(defproject leetconfig "0.0.4-SNAPHOST"
   :description "Leetconfig for neat clojure application configuration"
   :url "http://github.com/juhofriman/leetconfig"
   :license {:name "Eclipse Public License"
